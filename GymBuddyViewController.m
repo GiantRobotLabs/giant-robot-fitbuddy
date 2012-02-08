6f9b715f73ab2ac67efe3c0c86ca3a4188abe558
jneyer <social@jneyer.com>
2012-02-08 11:26:51 -0500

    Add workout path

--- GymBuddyViewController.m
+++ GymBuddyViewController.m
@@ -51,7 +51,8 @@
 
 -(void) loadDataFromExerciseObject
 {
-    //if (self.exercise.exercise_id == nil) self.exercise.exercise_id = 0;
+    if (self.exercise.exercise_id == nil) self.exercise.exercise_id = 0;
+    NSLog(@"set new property to %d", [self.exercise.exercise_id intValue]);
     self.nameLabel.text = self.exercise.name;
     self.setsLabel.text = self.exercise.sets;
     self.repsLabel.text = self.exercise.reps;
